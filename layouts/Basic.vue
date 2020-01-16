87c5dc45e401efe9bb423a8f5525296de75edf98
jjlmoya <jj.lopez@avoristravel.com>
2020-01-16 22:23:31 +0300

    Extrac basic

---
+++ layouts/Basic.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="">
+        <div class="left">
+            <div class="image">
+                <img src="">>
+            </div>
+            <div class="about">
+                <h2>About me</h2>
+                <p>Short text about me</p>
+            </div>
+            <div class="contact">
+                <h2>Contacto</h2>
+                <ul>
+                    <li>
+                        <i>i</i> <span>Option</span>
+                    </li>
+                    <li>
+                        <i>i</i> <span>Option</span>
+                    </li>
+                </ul>
+            </div>
+            <ul class="skills">
+                <li>
+                    <label>Skill1</label>
+                    <input
+                        type="range"
+                        max="100"
+                        min="0"
+                        value="50"
+                        disabled
+                    >
+                </li>
+            </ul>
+        </div>
+        <div class="right">
+            <div class="name"></div>
+            <div class="experience"></div>
+            <div class="grade"></div>
+            <div class="languages"></div>
+        </div>
+    </div>
+</template>
+<style lang="scss">@import 'default.scss';
+</style>
+<script>
+    export default {
+        name: 'DefaultLayout',
+        components: {
+        }
+    }
+</script>
