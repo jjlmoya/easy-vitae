cee1655da309d6de86de13539c9b4e532053b2a4
jjlmoya <jj.lopez@avoristravel.com>
2020-01-17 20:02:29 +0300

    update model

--- pages/cv/_id.vue
+++ pages/cv/_id.vue
@@ -1,54 +1,19 @@
 <template>
     <div class="cv">
         <Layout>
-            <div class="left">
-                <div class="image">
-                    <img src="">>
-                </div>
-                <div class="about">
-                    <h2>About me</h2>
-                    <p>Short text about me</p>
-                </div>
-                <div class="contact">
-                    <h2>Contacto</h2>
-                    <ul>
-                        <li>
-                            <i>i</i> <span>Option</span>
-                        </li>
-                        <li>
-                            <i>i</i> <span>Option</span>
-                        </li>
-                    </ul>
-                </div>
-                <ul class="skills">
-                    <li>
-                        <label>Skill1</label>
-                        <input
-                            type="range"
-                            max="100"
-                            min="0"
-                            value="50"
-                            disabled
-                        >
-                    </li>
-                </ul>
-            </div>
-            <div class="right">
-                <div class="name"></div>
-                <div class="experience"></div>
-                <div class="grade"></div>
-                <div class="languages"></div>
-            </div>
+            <Basic />
         </Layout>
     </div>
 </template>
 
 <script>
     import Layout from '@/layouts/Default.vue'
+    import Basic from '@/layouts/Basic.vue'
     export default {
         name: 'CV',
         components: {
-            Layout
+            Layout,
+            Basic
         },
         head () {
             return {
